5ef01ef4d09643af4482898a764b8c7a157307de
JooYoo <jooyoo@outlook.com>
2022-01-21 19:34:32 +0100

    create wesplit project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yu on 2022/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
