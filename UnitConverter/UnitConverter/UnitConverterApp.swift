669e79b51a4e0b3d20ad70039f45781d3081f806
JooYoo <jooyoo@outlook.com>
2022-01-22 17:36:05 +0100

    unit-converter: init project

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Yu on 2022/1/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
