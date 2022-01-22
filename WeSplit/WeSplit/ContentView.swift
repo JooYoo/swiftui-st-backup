e4d73dc8fa2c308c20e1526bc21245775ab4515f
JooYoo <jooyoo@outlook.com>
2022-01-22 17:21:16 +0100

    wesplit: add total-amount-section; add currency-format

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var currencyFormat: FloatingPointFormatStyle<Double>.Currency {
+        return .currency(code: Locale.current.currencyCode ?? "CNY")
+    }
+    
     var totalPerPerson: Double {
         let tip = checkAmount * (Double(tipPercentage) / 100)
         let res = (checkAmount + tip) / Double(numberOfPeople)
@@ -22,12 +26,17 @@ struct ContentView: View {
         return res
     }
     
+    var totalAmount: Double {
+        let tip = checkAmount * (Double(tipPercentage) / 100)
+        return checkAmount + tip
+    }
+    
     var body: some View {
         NavigationView{
             Form{
                 Section{
                     // checkAmount TextField
-                    TextField("Enter check amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
+                    TextField("Enter check amount", value: $checkAmount, format: currencyFormat)
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
@@ -41,18 +50,34 @@ struct ContentView: View {
                 
                 // tipPercentage Picker
                 Section{
+                    // segmented-style-picker
                     Picker("tip percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self){
                             Text($0, format: .percent)
                         }
                     }.pickerStyle(.segmented)
+                    // scroll-view-style-picker
+                    //                    Picker("tip percentage", selection: $tipPercentage){
+                    //                        ForEach(0..<101){
+                    //                            Text($0, format: .percent)
+                    //                        }
+                    //                    }
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+                // totleAmount
+                Section{
+                    Text(totalAmount, format: currencyFormat)
+                } header: {
+                    Text("Total cost")
+                }
+                
                 // totalPerPerson Text
                 Section{
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
+                    Text(totalPerPerson, format: currencyFormat)
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("WeSplit")
