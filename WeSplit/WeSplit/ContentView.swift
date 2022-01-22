4cb282491e8a54f49d6cf450dec4078cb3b1e5c7
JooYoo <jooyoo@outlook.com>
2022-01-22 14:32:11 +0100

    wesplit: get result display

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,12 +14,21 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let tip = checkAmount * (Double(tipPercentage) / 100)
+        let res = (checkAmount + tip) / Double(numberOfPeople)
+        
+        return res
+    }
+    
     var body: some View {
         NavigationView{
             Form{
                 Section{
+                    // checkAmount TextField
                     TextField("Enter check amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
                         .keyboardType(.decimalPad)
+                    // numberOfPeople Picker
                     Picker("Number of people", selection: $numberOfPeople){
                         ForEach(0..<99){
                             Text("\($0)")
@@ -27,18 +36,20 @@ struct ContentView: View {
                     }
                 }
                 
+                // tipPercentage Picker
                 Section{
                     Picker("tip percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self){
-                            Text("\($0)")
+                            Text($0, format: .percent)
                         }
                     }.pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+                // totalPerPerson Text
                 Section{
-                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
                 }
             }.navigationTitle("WeSplit")
         }
