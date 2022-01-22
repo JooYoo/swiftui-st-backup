8804f661f02fa044e8bf7eea5d00bc3f8da2c83f
JooYoo <jooyoo@outlook.com>
2022-01-22 13:10:45 +0100

    wesplit: add check-mount text-field

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form{
+            Section{
+                TextField("Enter check amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section{
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "CNY"))
+            }
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
