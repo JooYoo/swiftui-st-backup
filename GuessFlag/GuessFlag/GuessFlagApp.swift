87e1af09e40e886ab71e42144bd18d821549ccd7
JooYoo <jooyoo@outlook.com>
2022-01-24 16:48:35 +0100

    init

---
+++ GuessFlag/GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Yu on 2022/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
