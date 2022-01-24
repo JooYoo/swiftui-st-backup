bcbfb2e5560fbbada7b1044b9cb47a1cae5a070c
JooYoo <jooyoo@outlook.com>
2022-01-24 20:06:25 +0100

    guess-flag: Showing the player’s score with an alert

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         
@@ -27,17 +30,38 @@ struct ContentView: View {
                 
                 ForEach (0..<3) { number in
                     Button{
-                        // TODO: flag tapped
+                        // flag tapped
+                        flagTapped(number)
                     } label:{
                         Image(countries[number])
                             .renderingMode(.original)
                     }
                 }
             }
-            
-            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score: \(score).")
         }
         
+    }
+    
+    func flagTapped(_ number: Int)  {
+        if correctAnswer == number {
+            scoreTitle = "Correct"
+            score += 1
+        }else{
+            scoreTitle = "Wrong"
+            score -= 1
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries = countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
