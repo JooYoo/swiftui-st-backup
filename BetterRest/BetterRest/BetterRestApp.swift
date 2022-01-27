452a915c28a9097dfbfdcb28da9e537fdc3ce14d
JooYoo <jooyoo@outlook.com>
2022-01-27 19:20:33 +0100

    better-rest: init

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yu on 2022/1/27.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
