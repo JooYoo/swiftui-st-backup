68ffedb8091803975bc642968e70ee7366e144ff
JooYoo <jooyoo@outlook.com>
2022-01-28 17:15:38 +0100

    better-rest: Connecting SwiftUI to Core ML

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -1,16 +1,15 @@
-//
-//  ContentView.swift
-//  BetterRest
-//
-//  Created by Yu on 2022/1/27.
-//
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -31,10 +30,39 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK"){}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calculateBedtime() {
+        do {
+            // config Model
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            // get wakeUp time as seconds
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            // get prediction
+            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            // res
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            // display res in alert
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry error"
+        }
+        showingAlert = true
     }
 }
 
