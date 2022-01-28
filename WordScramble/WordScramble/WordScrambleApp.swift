0bcc688230bbe89f2b5fc3851449b32562ce2da4
JooYoo <jooyoo@outlook.com>
2022-01-28 19:30:38 +0100

    word-scramble: init

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yu on 2022/1/28.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
