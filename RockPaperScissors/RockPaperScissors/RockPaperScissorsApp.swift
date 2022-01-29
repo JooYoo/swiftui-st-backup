c1c5637183cef925aa5a45eb394cd7a0b3cf633e
JooYoo <jooyoo@outlook.com>
2022-01-29 05:20:44 +0100

    rock-paper-scissors: init

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Yu on 2022/1/29.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
