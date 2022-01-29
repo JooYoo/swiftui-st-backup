92d54eb2b5c879d66fd9273937fe8eb579bb51a2
JooYoo <jooyoo@outlook.com>
2022-01-29 07:42:59 +0100

    rock-paper-scissors: basic ui

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rpc = ["✊🏼", "✋🏼", "✌🏼"].shuffled()
+    
+    @State private var aiChoiceIdx = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    // TODO: user choose Index
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            // win: red: 0.02, green: 0.77, blue: 0.42
+            // lose: red: 1.00, green: 0.42, blue: 0.51
+            Color(red: 0.02, green: 0.77, blue: 0.42)
+                .ignoresSafeArea()
+        
+        VStack{
+            
+            Spacer()
+            
+            Text(rpc[aiChoiceIdx])
+                .font(.system(size: 120))
+                .padding(.vertical,20)
+                .padding(.horizontal,80)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+            Spacer()
+            
+            Text("You should \(shouldWin ? "WIN" : "LOSE")")
+                .font(.system(size: 40))
+                .fontWeight(.heavy)
+            
+            Spacer()
+            
+            HStack{
+                Button("✊🏼"){}
+                Button("✌🏼"){}
+                Button("✋🏼"){}
+            }
+            .font(.system(size: 90))
+            .buttonStyle(.bordered)
+            
+            Text("SCORE: \(score)")
+                .font(.system(size: 30))
+                .fontWeight(.semibold)
+                .padding(.top, 20)
+            
+        }
+        .padding()
+        .foregroundColor(.white)
+            
+        }
     }
 }
 
