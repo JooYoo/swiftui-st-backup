61433575513d51b974153dcf7b499f48f7dd565b
JooYoo <jooyoo@outlook.com>
2022-01-29 11:59:36 +0100

    word-scramble: adding to a list of words

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = "RootWord"
+    @State private var newWord = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                }
+                
+                Section{
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .onSubmit(addNewWord)
+            .navigationTitle(rootWord)
+        }
     }
+    
+    func addNewWord(){
+        
+        // process user input
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // prevent input empty
+        guard answer.count > 0 else { return }
+        
+        // add to list
+        usedWords.insert(answer, at: 0)
+        
+        // reset TextField
+        newWord = ""
+    }
+    
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
