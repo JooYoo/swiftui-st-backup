f1142ad5059e3f5ca745cb3c35ed25d35ada9f4a
JooYoo <jooyoo@outlook.com>
2022-01-30 11:41:49 +0100

    i-expense: init

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yu on 2022/1/30.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
