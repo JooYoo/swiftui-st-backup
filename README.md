aa01b2728257ac08208bf6a88e29b34d254a20b7
JooYoo <jooyoo@outlook.com>
2022-01-31 17:20:50 +0100

    i-expense: locale currency; amount color; type sections

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # iExpense
+
+TODO:
+
+- [ ] add `onDelete()` for both Sections which have different type
--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,25 +7,63 @@
 
 import SwiftUI
 
+struct SectionForEach: View {
+    var items: [ExpenseItem]
+    
+    var body: some View {
+        ForEach(items, id: \.id){ item in
+            HStack{
+                VStack(alignment: .leading){
+                    Text(item.name)
+                        .font(.headline)
+                    Text(item.type)
+                }
+                Spacer()
+                Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .foregroundColor(item.amount<10 ? .green : item.amount<100 ? .yellow : .red)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @StateObject var expenses = Expenses()
     @State private var showingAddExpense = false
     
+    private var businessItems: [ExpenseItem] {
+        return expenses.items.filter { $0.type == "Business" }
+    }
+    private var personalItems: [ExpenseItem] {
+        return expenses.items.filter { $0.type == "Personal" }
+    }
+    
     var body: some View {
         NavigationView{
             List{
-                ForEach(expenses.items, id: \.id){ item in
-                    HStack{
-                        VStack(alignment: .leading){
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
+                // TODO: remove this section
+                Section{
+                    ForEach(expenses.items, id: \.id){ item in
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type)
+                            }
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(item.amount<10 ? .green : item.amount<100 ? .yellow : .red)
                         }
-                        Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
                     }
+                    .onDelete(perform: removeItem)
                 }
-                .onDelete(perform: removeItem)
+                // FIXME: add onDelete
+                Section {
+                    SectionForEach(items: personalItems)
+                }
+                // FIXME: add onDelete
+                Section{
+                    SectionForEach(items: businessItems)
+                }
             }
             .navigationTitle("iExpense")
             .toolbar {
