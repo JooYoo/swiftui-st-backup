d757c06059fff37d8a70f5c1c28bd57e2a9587c0
JooYoo <jooyoo@outlook.com>
2022-01-31 17:27:13 +0100

    moon-shot: init

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Yu on 2022/1/31.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
