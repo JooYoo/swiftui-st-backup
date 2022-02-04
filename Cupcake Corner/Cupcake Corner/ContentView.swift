be7aa6edf6268a474a87680e4ecd44cdbc0647fc
JooYoo <jooyoo@outlook.com>
2022-02-04 18:09:24 +0100

    cupcake-corner: Taking basic order details

--- Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var order = Order()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices){ i in
+                            Text(Order.types[i])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section{
+                    Toggle("Any special request?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section{
+                    NavigationLink{
+                        AddressView(order: order)
+                    }label: {
+                        Text("Delivery details")
+                    }
+                }
+                .navigationBarTitle("Cupcake")
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
---
+++ Cupcake Corner/Cupcake Corner/Order.swift
@@ -0,0 +1,24 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Yu on 2022/2/4.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    @Published var type = 0
+    @Published var quantity = 3
+    @Published var specialRequestEnabled = false{
+        didSet{
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
