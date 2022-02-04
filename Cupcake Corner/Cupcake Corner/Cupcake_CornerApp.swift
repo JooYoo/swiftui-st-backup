efb77f591b664a540d6356e74f8f6ea446ad7150
JooYoo <jooyoo@outlook.com>
2022-02-04 17:27:42 +0100

    cupcake-corner: init

---
+++ Cupcake Corner/Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Yu on 2022/2/4.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
