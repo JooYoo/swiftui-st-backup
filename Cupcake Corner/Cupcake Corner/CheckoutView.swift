ac8ef0dd459a768a5697042c4b4a20936a65b29e
JooYoo <jooyoo@outlook.com>
2022-02-05 10:41:34 +0100

    cupcake-corner: Sending and receiving orders over the internet

--- Cupcake Corner/Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/Cupcake Corner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView{
             VStack{
@@ -25,12 +28,49 @@ struct CheckoutView: View {
                 Text("Your total cost: \(order.cost, format: .currency(code: Locale.current.currencyCode ?? "USD"))")
                     .font(.title)
                 
-                Button("Place order"){}
+                Button("Place order"){
+                    Task{
+                        await placeOrder()
+                    }
+                }
                 .padding()
             }
         }
         .navigationTitle("Checkout")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order Success", isPresented: $showingConfirmation) {
+            Button("Ok"){}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        
+        // MARK: - POST
+        // encode order => JSON obj
+        guard let encoded = try? JSONEncoder().encode(order) else{
+            print("encode failed")
+            return
+        }
+        // get request ready
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        // send request
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            // decode respinse data
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            // create alert
+            confirmationMessage = "You ordered \(Order.types[decodedOrder.type]) cake x \(decodedOrder.quantity)"
+            showingConfirmation = true
+            
+        } catch {
+            print("send request error")
+        }
     }
 }
 
