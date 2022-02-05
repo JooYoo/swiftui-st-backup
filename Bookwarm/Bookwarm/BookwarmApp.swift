e4d3866fb3bb5290b8d0c630f1945a08bc463269
JooYoo <jooyoo@outlook.com>
2022-02-05 11:32:20 +0100

    bookwarm: init

---
+++ Bookwarm/Bookwarm/BookwarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwarmApp.swift
+//  Bookwarm
+//
+//  Created by Yu on 2022/2/5.
+//
+
+import SwiftUI
+
+@main
+struct BookwarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
