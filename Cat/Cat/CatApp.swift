0e2f91b809dd40176e1089575325507f3aa6dc1b
JooYoo <jooyoo@outlook.com>
2022-02-09 19:31:55 +0100

    cat: init

---
+++ Cat/Cat/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Cat
+//
+//  Created by Yu on 2022/2/9.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
