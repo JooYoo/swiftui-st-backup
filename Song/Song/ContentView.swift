1612f8537dddf6f32bd5a9ad9e76301cb9c7d7cb
JooYoo <jooyoo@outlook.com>
2022-03-27 18:02:03 +0200

    song: init

---
+++ Song/Song/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Song
+//
+//  Created by Yu on 2022/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Song/Song/SongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongApp.swift
+//  Song
+//
+//  Created by Yu on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct SongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
