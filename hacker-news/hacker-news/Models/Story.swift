d13929d855320b9cdd6b6ccbc358eb4681d0f212
Yu Zhu <yu.zhu@liebherr.com>
2022-10-18 19:00:05 +0200

    retrieve Story by ID

---
+++ hacker-news/hacker-news/Models/Story.swift
@@ -0,0 +1,14 @@
+//
+//  Story.swift
+//  hacker-news
+//
+//  Created by Yu on 18.10.22.
+//
+
+import Foundation
+
+struct Story: Codable {
+    let id: Int
+    let title: String
+    let url: String
+}
--- hacker-news/hacker-news/Services/Webservice.swift
+++ hacker-news/hacker-news/Services/Webservice.swift
@@ -10,6 +10,23 @@ import Combine
 
 class Webservice {
     
+    func getStoryBy(storyId: Int) -> AnyPublisher<Story, Error> {
+        guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(storyId).json?print=pretty") else {
+            fatalError("url invalid")
+        }
+        
+        let publisher = URLSession
+            .shared
+            .dataTaskPublisher(for: url)
+            .receive(on: RunLoop.main)
+            .map(\.data)
+            .decode(type: Story.self, decoder: JSONDecoder())
+            .catch{ _ in Empty<Story, Error>()}
+            .eraseToAnyPublisher()
+        
+        return publisher
+    }
+    
     func getAllTopStories() -> AnyPublisher<[Int], Error> {
         guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty") else {
             fatalError("Invalid URL")
