339a7b39e9cad10e98ffbd51dbff51d70dbe1ce0
Yu Zhu <yu.zhu@liebherr.com>
2022-10-19 06:28:43 +0200

    StoryDetailViewModel

---
+++ hacker-news/hacker-news/ViewModels/StoryDetailViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  StoryDetailViewModel.swift
+//  hacker-news
+//
+//  Created by Yu on 19.10.22.
+//
+
+import Foundation
+import Combine
+
+class StoryDetailViewModel: ObservableObject {
+    @Published var story: Story!
+    private var cancellable: AnyCancellable?
+    var storyId: Int
+    
+    init(storyId: Int) {
+        self.storyId = storyId
+        self.cancellable = Webservice()
+            .getStoryBy(storyId: self.storyId)
+            .sink(receiveCompletion: {_ in }, receiveValue: { story in
+                self.story = story
+            })
+    }
+}
+
+extension StoryDetailViewModel {
+    var title: String {
+        return self.story.title
+    }
+    
+    var url: String {
+        return self.story.url
+    }
+}
